acc93a402244f469aab874b47a66d96e920b3076
mwanakijiji <eckhart.spalding@sydney.edu.au>
2024-03-15 17:02:28 +1100

    classes not working quite yet; debug

--- notebooks/scratch_extraction.py
+++ notebooks/scratch_extraction.py
@@ -2,14 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from utils import fcns, backbone_classes
 import time
+import configparser
 
-import ipdb; ipdb.set_trace()
 
 if __name__ == "__main__":
 
     start_time = time.time()
 
     stem = '/Users/bandari/Documents/git.repos/GLINT_reduction_v3/data/'
+
+    # Read the config file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    # Get the values from the config file
+    #variable1 = config.get('section_name', 'variable1')
+
+    # Use the variables in your code
+    # For example:
+    # stem = variable1
+    # abs_pos_00 = variable2
+
+    # spectrum starting positions (absolute coordinates, arbit. number of spectra)
+    abs_pos_00 = {'0':(0,177),
+        '1':(0,159),
+        '2':(0,102)}
 
     # true data
     test_frame = fcns.read_fits_file(stem + 'sample_data_3_spec.fits')
@@ -27,31 +43,25 @@ if __name__ == "__main__":
     test_data_slice += (1e-3)*np.random.rand(np.shape(test_data_slice)[0],np.shape(test_data_slice)[1])
     test_variance_slice += (1e-3)*np.random.rand(np.shape(test_variance_slice)[0],np.shape(test_variance_slice)[1])
     '''
-    
-    #import ipdb; ipdb.set_trace()
 
+    # initialize basic spectrum object which contains spectra info
+    spec_obj = backbone_classes.SpecData(num_spec = len(abs_pos_00), len_spec = 200)
 
-    # initialize dictionaries which will store the spectrum profiles
-    ## THIS SETS THE NUMBER OF SPECTRA TO BE EXTRACTED
-    abs_pos, eta_flux, wavel_solns = fcns.infostack(x_extent=np.shape(test_data_slice)[1], 
-                                                          y_extent=np.shape(test_data_slice)[0])
+    # instantiate extraction machinery
+    extractor_instance = backbone_classes.Extractor(num_spec = len(abs_pos_00), len_spec = 200)
+    # generate a profile for each spectrum, and put into the spec_obj
+    spec_obj.dict_profiles = extractor_instance.stacked_profiles(array_shape=np.shape(test_data_slice), abs_pos=abs_pos_00)
 
-    dict_profiles = {}
+    import ipdb; ipdb.set_trace()  
 
-    # generate a profile for each spectrum
-    for spec_num in range(0,len(eta_flux)):
-        dict_profiles[str(spec_num)] = fcns.simple_profile(array_shape = np.shape(test_data_slice), 
-                                                                 x_left=abs_pos[str(spec_num)][0], 
-                                                                 y_left=abs_pos[str(spec_num)][1], 
-                                                                 len_spec=200, 
-                                                                 sigma_pass=1)
-        # add a little bit of noise for troubleshooting
-        #dict_profiles[str(spec_num)] += (1e-3)*np.random.rand(np.shape(dict_profiles[str(spec_num)])[0],np.shape(dict_profiles[str(spec_num)])[1])
-        
-    
-        
+    spec_obj.spec_flux = extractor_instance.extract_spectra(x_extent=np.shape(test_data_slice)[1], 
+                                          y_extent=np.shape(test_data_slice)[0],
+                                          eta_flux=spec_obj.spec_flux, 
+                                          dict_profiles=spec_obj.dict_profiles, 
+                                          D=test_data_slice, 
+                                          array_variance=test_variance_slice)
 
-    #import ipdb; ipdb.set_trace()
+    '''
     # extract the spectra and put them into dictionary
     eta_flux = fcns.extract_spectra(x_extent=np.shape(test_data_slice)[1], 
                                           y_extent=np.shape(test_data_slice)[0], 
@@ -59,7 +69,7 @@ if __name__ == "__main__":
                                           dict_profiles=dict_profiles, 
                                           D=test_data_slice, 
                                           array_variance=test_variance_slice)
-
+    '''
     print(type(eta_flux))
     end_time = time.time()
     execution_time = end_time - start_time
