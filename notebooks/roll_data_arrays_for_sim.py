94aea0f5430c76fffde88e012476bfcfe2a93454
mwanakijiji <eckhart.spalding@sydney.edu.au>
2024-03-20 16:07:00 +1100

    extracts spectra in loop

---
+++ notebooks/roll_data_arrays_for_sim.py
@@ -0,0 +1,44 @@
+# To simulate data, takes a sample array and translates it small amounts and adds different amounts of noise 
+
+import configparser
+from utils import fcns
+from astropy.io import fits
+import numpy as np
+import scipy
+
+stem = '/Users/bandari/Documents/git.repos/GLINT_reduction_v3/data/'
+stem_write = stem + 'fake_data/'
+
+# true data
+test_frame = fcns.read_fits_file(stem + 'sample_data_3_spec.fits')
+test_data_slice = test_frame[0,:,:]
+test_variance_slice = test_frame[1,:,:]
+
+import ipdb; ipdb.set_trace()
+
+# loop over number of frames to generate
+num_frames = 20
+
+for i in range(0,num_frames):
+
+    frame_init = test_frame # will be overwritten
+    
+    # small x,y offsets
+    xoff = np.random.normal()
+    yoff = np.random.normal()
+
+    test_data_slice_shifted = scipy.ndimage.shift(test_data_slice, (-yoff, -xoff), mode='nearest')
+    test_variance_slice_shifted = scipy.ndimage.shift(test_data_slice, (-yoff, -xoff), mode='nearest')
+
+    # add some white noise
+    test_data_slice_shifted += 5.*np.random.normal(np.shape(test_data_slice_shifted)[0],np.shape(test_data_slice_shifted)[1])
+    test_variance_slice_shifted += 1.*np.random.normal(np.shape(test_variance_slice_shifted)[0],np.shape(test_variance_slice_shifted)[1])
+
+    frame_init[0,:,:] = test_data_slice_shifted
+    frame_init[1,:,:] = test_variance_slice_shifted
+
+    # save
+    file_name = stem_write + f'fake_data_{i:02d}.fits'
+    fits.writeto(file_name, frame_init, overwrite=True)
+    print('Wrote',file_name)
+    
--- notebooks/scratch_extraction.py
+++ notebooks/scratch_extraction.py
@@ -3,6 +3,7 @@ import numpy as np
 from utils import fcns, backbone_classes
 import time
 import configparser
+from astropy.io import fits
 
 ## ## TBD: make clearer distinction between length of spectra, and that of extraction profile
 
@@ -10,7 +11,8 @@ if __name__ == "__main__":
 
     start_time = time.time()
 
-    stem = '/Users/bandari/Documents/git.repos/GLINT_reduction_v3/data/'
+    #dir_read = '/Users/bandari/Documents/git.repos/GLINT_reduction_v3/data/'
+    dir_read = '/Users/bandari/Documents/git.repos/GLINT_reduction_v3/data/fake_data/'
 
     # Read the config file
     config = configparser.ConfigParser()
@@ -30,6 +32,7 @@ if __name__ == "__main__":
         '3':(0,72)}
 
     # true data
+    stem = '/Users/bandari/Documents/git.repos/GLINT_reduction_v3/data/'
     test_frame = fcns.read_fits_file(stem + 'sample_data_3_spec.fits')
     test_data_slice = test_frame[0,:,:]
     test_variance_slice = test_frame[1,:,:]
@@ -44,35 +47,43 @@ if __name__ == "__main__":
     test_variance_slice += (1e-3)*np.random.rand(np.shape(test_variance_slice)[0],np.shape(test_variance_slice)[1])
     '''
 
-    # initialize basic spectrum object which contains spectra info
-    spec_obj = backbone_classes.SpecData(num_spec = len(abs_pos_00), 
-                                         len_spec = np.shape(test_data_slice)[1], 
-                                         sample_frame = test_data_slice)
+    # retrieve list of files to operate on
+    file_list = fcns.file_list_maker(dir_read)
 
-    # instantiate extraction machinery
-    extractor = backbone_classes.Extractor(num_spec = len(abs_pos_00), 
-                                           len_spec = np.shape(test_data_slice)[1])
-    
-    # generate a profile for each spectrum, and update the spec_obj with them
-    ## ## TODO: SUBSUME THE test_data_slice INTO THE SPEC_OBJ
-    ## ## TODO: SOME OF THE LENGTHY ARGUMENT LISTS OF THESE FUNCTION CALLS CAN PROBABLY BE PRUNED
-    extractor.stacked_profiles(target_instance=spec_obj,
-                                        abs_pos=abs_pos_00)
+    # loop over all data files
+    for file_num in range(0,len(file_list)):
 
-    import ipdb; ipdb.set_trace()  
+        # read in image
+        hdul = fits.open(file_list[file_num])
 
-    # do the actual spectral extraction
-    extractor.extract_spectra(target_instance=spec_obj,
-                                          D=test_data_slice, 
-                                          array_variance=test_variance_slice)
+        readout_data = hdul[0].data[0,:,:]
+        readout_variance = hdul[0].data[1,:,:]
+
+        # initialize basic spectrum object which contains spectra info
+        spec_obj = backbone_classes.SpecData(num_spec = len(abs_pos_00), 
+                                            len_spec = np.shape(test_data_slice)[1], 
+                                            sample_frame = test_data_slice)
+
+        # instantiate extraction machinery
+        extractor = backbone_classes.Extractor(num_spec = len(abs_pos_00),
+                                            len_spec = np.shape(test_data_slice)[1])
+        
+        # generate a profile for each spectrum, and update the spec_obj with them
+        extractor.stacked_profiles(target_instance=spec_obj,
+                                            abs_pos=abs_pos_00)
+
+        # do the actual spectral extraction
+        extractor.extract_spectra(target_instance=spec_obj,
+                                            D=readout_data, 
+                                            array_variance=readout_variance)
 
 
-    print(type(spec_obj.spec_flux))
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print("Execution time:", execution_time, "seconds")
+        print(type(spec_obj.spec_flux))
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print("Execution time:", execution_time, "seconds")
 
-    for i in range(0,len(spec_obj.spec_flux)):
-        plt.plot(spec_obj.spec_flux[str(i)])
-        plt.show()
+        for i in range(0,len(spec_obj.spec_flux)):
+            plt.plot(spec_obj.spec_flux[str(i)])
+            plt.show()
 
